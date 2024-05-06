e631e416dca6c0035286672155a046d0d4a03c89
Lamenkazu <333.erick@gmail.com>
2024-05-06 13:16:43 -0300

    ⚙️ Layouts da aplicação

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,14 +2,20 @@ import { createBrowserRouter } from "react-router-dom";
 
 import { Dashboard } from "./pages/app/Dashboard";
 import { SignIn } from "./pages/auth/SignIn";
+import { AppLayout } from "./pages/_layouts/App";
+import { AuthLayout } from "./pages/_layouts/Auth";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <Dashboard />,
+    element: <AppLayout />,
+    children: [{ path: "/", element: <Dashboard /> }],
   },
   {
-    path: "/sign-in",
-    element: <SignIn />,
+    path: "/",
+    element: <AuthLayout />,
+    children: [{ path: "/sign-in", element: <SignIn /> }],
   },
+
+  // ,
 ]);
