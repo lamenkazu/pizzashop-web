731a8a828abee6cd67e4bb4ce81b8c1d50066c32
Lamenkazu <333.erick@gmail.com>
2024-05-28 21:35:18 -0300

    ✨ Métricas no Dashboard

--- src/pages/app/dashboard/month-orders-amount-card.tsx
+++ src/pages/app/dashboard/month-orders-amount-card.tsx
@@ -1,8 +1,14 @@
+import { useQuery } from '@tanstack/react-query'
 import { Utensils } from 'lucide-react'
 
+import { getMonthOrdersAmount } from '@/api/get-month-orders-amount'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 
 export const MonthOrdersAmountCard = () => {
+  const { data: monthOrdersAmount } = useQuery({
+    queryKey: ['metrics', 'month-orders-amount'],
+    queryFn: getMonthOrdersAmount,
+  })
   return (
     <Card className="bg-background">
       <CardHeader className="flex-row items-center justify-between space-y-0 pb-2">
@@ -11,11 +17,25 @@ export const MonthOrdersAmountCard = () => {
       </CardHeader>
 
       <CardContent className="space-y-1">
-        <span className="text-2xl font-bold tracking-tight">246 pedidos</span>
-        <p className="text-xs text-muted-foreground">
-          <span className="text-emerald-500 dark:text-emerald-400">+6%</span> em
-          relação ao mês passado
-        </p>
+        {monthOrdersAmount && (
+          <>
+            <span className="text-2xl font-bold tracking-tight">
+              {monthOrdersAmount.amount.toLocaleString('pt-BR')}
+            </span>
+            <p className="text-xs text-muted-foreground">
+              {monthOrdersAmount.diffFromLastMonth >= 0 ? (
+                <span className="text-emerald-500 dark:text-emerald-400">
+                  +{monthOrdersAmount.diffFromLastMonth}%
+                </span>
+              ) : (
+                <span className="text-rose-500 dark:text-rose-400">
+                  {monthOrdersAmount.diffFromLastMonth}%
+                </span>
+              )}{' '}
+              em relação ao mês passado
+            </p>
+          </>
+        )}
       </CardContent>
     </Card>
   )
