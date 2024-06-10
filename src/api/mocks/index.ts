742afe5110d323e28b27fd66b9605d9405a7f051
Lamenkazu <333.erick@gmail.com>
2024-06-10 19:16:19 -0300

    ✨ Mock de Sign Up

--- src/api/mocks/index.ts
+++ src/api/mocks/index.ts
@@ -2,9 +2,10 @@ import { setupWorker } from 'msw/browser'
 
 import { env } from '@/env'
 
+import { registerRestaurantMock } from './register-restaurant-mock'
 import { signInMock } from './sign-in-mock'
 
-export const worker = setupWorker(signInMock)
+export const worker = setupWorker(signInMock, registerRestaurantMock)
 
 export const enableMSW = async () => {
   if (env.MODE !== 'test') {
