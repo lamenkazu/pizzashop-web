bbc380ce1830d2b53b36dde05bd22b9943ad224c
Lamenkazu <333.erick@gmail.com>
2024-06-11 09:20:37 -0300

    ✨ Mock dos Pedidos e Detalhes dos Pedidos

---
+++ src/api/mocks/get-order-details-mock.ts
@@ -0,0 +1,42 @@
+import { http, HttpResponse } from 'msw'
+
+import {
+  GetOrderDetailsParams,
+  GetOrderDetailsResponse,
+} from '../get-order-details'
+
+export const getOrderDetailsMock = http.get<
+  GetOrderDetailsParams,
+  never,
+  GetOrderDetailsResponse
+>('/orders/:orderId', ({ params }) => {
+  return HttpResponse.json({
+    id: params.orderId,
+    customer: {
+      name: 'Jane Doe',
+      email: 'janedoe@mail.com',
+      phone: '31992922940',
+    },
+    status: 'pending',
+    createdAt: new Date().toISOString(),
+    orderItems: [
+      {
+        id: 'order-item-1',
+        priceInCents: 1000,
+        product: {
+          name: 'Pizza Pepperoni',
+        },
+        quantity: 1,
+      },
+      {
+        id: 'order-item-1',
+        priceInCents: 2000,
+        product: {
+          name: 'Pizza Marguerita',
+        },
+        quantity: 2,
+      },
+    ],
+    totalInCents: 3000,
+  })
+})
